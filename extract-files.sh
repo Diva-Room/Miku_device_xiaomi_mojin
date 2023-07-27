2ff90f6e21d20b6debfe0e6005b16e2785763e0b
AmeChanRain <downloadbot007@gmail.com>
2023-07-27 12:45:11 +0000

    mona: Add zijin specific blobs

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ function blob_fixup() {
         vendor/etc/camera/mona_motiontuning.xml)
             sed -i 's/xml=version="1.0"/xml version="1.0"/g' "${2}"
             ;;
+        vendor/etc/camera/zijin_motiontuning.xml)
+            sed -i 's/xml=version="1.0"/xml version="1.0"/g' "${2}"
+            ;;
         vendor/etc/camera/pureShot_parameter.xml \
         |vendor/etc/camera/pureView_parameter.xml)
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
