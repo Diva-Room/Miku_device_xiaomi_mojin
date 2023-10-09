51030acc087fbc4101f1365633c785948c12602f
AmeChanRain <downloadbot007@gmail.com>
2023-10-09 08:37:45 +0000

    mojin: Switch to AIDL wifi service

--- platform/wifi_features.mk
+++ platform/wifi_features.mk
@@ -5,7 +5,7 @@
 #
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     libwifi-hal-ctrl \
