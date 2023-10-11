00af81f56f7b522c03056eaf31abf45b5c2a1b7b
AmeChanRain <downloadbot007@gmail.com>
2023-10-11 03:08:44 +0000

    mojin: Move NFC configs to device configuration

--- platform/nfc.mk
+++ platform/nfc.mk
@@ -12,3 +12,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc_snxxx@1.2-service \
     libchrome.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf
