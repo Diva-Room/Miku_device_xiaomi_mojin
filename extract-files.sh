543122e6dc1a1dc8d1da75ecc93467ea2a8cc8c6
AmeChanRain <downloadbot007@gmail.com>
2024-04-08 14:37:10 +0800

    mojin: Drop Dolby Vision Codec hacks
    Change-Id: I6537cf06eddf054220ea01d6b7845a414d7a4f9a

--- extract-files.sh
+++ extract-files.sh
@@ -60,10 +60,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/bin/hw/dolbycodec2)
-            patchelf --add-needed "libshim.so" "${2}"
-            patchelf --replace-needed libcodec2_hidl@1.0.so libcodec2_hidl@1.0.stock.so "${2}"
-            ;;
         vendor/etc/media_lahaina/video_system_specs.json \
         |vendor/etc/media_shima_v1/video_system_specs.json \
         |vendor/etc/media_yupik_v1/video_system_specs.json)
@@ -71,13 +67,6 @@ function blob_fixup() {
             ;;
         vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
             sed -ni '/ozoaudio/!p' "${2}"
-            ;;
-        vendor/lib/libcodec2_hidl@1.0.stock.so)
-            patchelf --set-soname libcodec2_hidl@1.0.stock.so "${2}"
-            patchelf --replace-needed libcodec2_vndk.so libcodec2_vndk.stock.so "${2}"
-            ;;
-        vendor/lib/libcodec2_vndk.stock.so)
-            patchelf --set-soname libcodec2_vndk.stock.so "${2}"
             ;;
     esac
 }
--- platform/media.mk
+++ platform/media.mk
@@ -33,5 +33,3 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libstagefrighthw
 
-# Shim for Dolby codec
-PRODUCT_PACKAGES += libshim
