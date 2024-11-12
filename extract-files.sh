9d9851b71867844ced4f9c0c0c9cd5eddaffc818
Giovanni Ricca <giovanniricca@proton.me>
2024-11-12 16:22:26 +0100

    mojin: Move to 64bit wfdservice
    Co-authored-by: Alexander Koskovich <zvnexus@outlook.com>
    Co-authored-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: I9d1e97ea8ac8cc75bdb6c29d6dfc0008e57a660d

--- extract-files.sh
+++ extract-files.sh
@@ -62,7 +62,11 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        system_ext/lib64/libwfdservice.so|system_ext/lib/libwfdservice.so)
+        system_ext/etc/init/wfdservice.rc)
+            sed -i 's/start wfdservice/start wfdservice64/g' "${2}"
+            sed -i 's/stop wfdservice/stop wfdservice64/g' "${2}"
+            ;;
+        system_ext/lib64/libwfdservice.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
             "${PATCHELF}" --replace-needed "android.media.audio.common.types-V3-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
