cd5f1abc0d979d14b207f8734d1f9e3896fe6837
AmeChanRain <downloadbot007@gmail.com>
2025-03-26 18:03:46 +0800

    fixup! mojin: Patch libwfdservice to resolve duplicate dependencies

--- extract-files.sh
+++ extract-files.sh
@@ -64,7 +64,8 @@ function blob_fixup() {
     case "${1}" in
         system_ext/lib64/libwfdservice.so|system_ext/lib/libwfdservice.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V3-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
             ;;
         vendor/etc/media_lahaina/video_system_specs.json \
         |vendor/etc/media_shima_v1/video_system_specs.json \
