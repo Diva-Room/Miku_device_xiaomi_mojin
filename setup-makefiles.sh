9911a5b2f70155a4fdcb9492e488f3e33f2b5c2b
AmeChanRain <downloadbot007@gmail.com>
2025-03-26 16:30:17 +0800

    mojin: Add perf into namespace
    * For boost framework
    
    Change-Id: I919ea70899efa17c17606c51546a42c6ddf2fa6b

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -31,6 +31,7 @@ function vendor_imports() {
 		"vendor/qcom/opensource/commonsys/display",
 		"vendor/qcom/opensource/commonsys-intf/display",
 		"vendor/qcom/opensource/dataservices",
+		"vendor/qcom/perf",
 EOF
 }
 
